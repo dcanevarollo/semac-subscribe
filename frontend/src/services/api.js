54db5844dd1925ea0e2952136e2e9b16b77705ea
Douglas Canevarollo <douglascanevarollo@gmail.com>
2019-08-16 11:27:35 -0300

    Remove app functions of the API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://semac-inscriptions.web.app"
+  baseURL: "https://us-central1-semac-inscriptions.cloudfunctions.net/httpServer"
 });
 
 export default api;
