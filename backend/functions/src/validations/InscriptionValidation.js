80dfa4b46d0ea85099f2e7431e9b3540c3ea237c
Douglas Canevarollo <douglascanevarollo@gmail.com>
2019-08-17 13:07:43 -0300

    Fix the CPF validation algorithm

--- backend/functions/src/validations/InscriptionValidation.js
+++ backend/functions/src/validations/InscriptionValidation.js
@@ -18,15 +18,28 @@ module.exports = {
     
         let sum = 0, rest;
 
-        if (strCpf == '00000000000') {
+        /* Consideramos, primeiro, CPFs de sequências de números iguais como erro. */
+        if (strCpf == '00000000000' 
+            || strCpf == '11111111111' 
+            || strCpf == '22222222222' 
+            || strCpf == '33333333333'
+            || strCpf == '44444444444' 
+            || strCpf == '55555555555' 
+            || strCpf == '66666666666' 
+            || strCpf == '77777777777' 
+            || strCpf == '88888888888'
+            || strCpf == '99999999999') {
             return false;
         }
 
+        /* Verificação do 1º dígito verificador. */
         for (i = 1; i <= 9; i++) {
             sum += parseInt(strCpf.substring(i - 1, i)) * (11 - i);
         }
+        console.log(sum);
 
         rest = (sum * 10) % 11;
+        console.log(rest)
 
         if (rest == 10 || rest == 11) {
             rest = 0;
@@ -36,6 +49,7 @@ module.exports = {
             return false;
         }
 
+        /* Verificação do 2º dígito verificador. */
         sum = 0;
         for (i = 1; i <= 10; i++) {
             sum += parseInt(strCpf.substring(i - 1, i)) * (12 - i);
